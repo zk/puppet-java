b0ff0a6a00ea8486f8604b1273013c7508c4d877
zk <zachary.kim@gmail.com>
2013-06-26 19:28:33 -0700

    Add puppet file defs for sec dir, local & us policy files

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,19 @@ class java {
   $jdk_url = 'https://s3.amazonaws.com/boxen-downloads/java/jdk-7u21-macosx-x64.dmg'
   $wrapper = "${boxen::config::bindir}/java"
 
+
+  # Allow 'large' keys locally.
+  $java_home = `/usr/libexec/java_home`
+  $sec_dir = "#{java_homme}/lib/security"
+
+  # !! TODO !! put in s3
+  $local_policy_url = 'http://cl.ly/1I3t1K1Q0G1N/download/local_policy.jar'
+  $local_policy_path = "#{java_home}/lib/security/local_policy.jar"
+
+  # !! TODO !! put in s3
+  $us_policy_url = 'http://cl.ly/2M0s2l3v1x2m/download/US_export_policy.jar'
+  $us_policy_path = "#{java_home}/lib/security/US_policy.jar"
+
   package {
     'jre-7u21.dmg':
       ensure   => present,
@@ -20,5 +33,27 @@ class java {
     source  => 'puppet:///modules/java/java.sh',
     mode    => 0755,
     require => Package['java']
+  }
+
+  file { $sec_dir:
+    ensure  => 'directory',
+    owner   => 'root',
+    mode    => 0775,
+    alias   => 'sec-dir',
+    require => Package['java']
+  }
+
+  file { $local_policy_path:
+    source  => $local_policy_url,
+    owner   => 'root',
+    mode    => 0664,
+    require => File['sec-dir']
+  }
+
+  file { $local_policy_path:
+    source  => $local_policy_url,
+    owner   => 'root',
+    mode    => 0664,
+    require => File['sec-dir']
   }
 }
